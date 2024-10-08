11741da98834e263d5ead43ba5c8459dfcc629c9
Mika Qiao <36143149+MikaQiao1218@users.noreply.github.com>
2024-10-08 10:09:18 +0800

    Add files via upload

---
+++ 4.11小恐龙网页/static/js/babel-polyfill.7f622118fedff873caa8.js
@@ -0,0 +1,2 @@
+webpackJsonp([3],[],["j1ja"]);
+//# sourceMappingURL=babel-polyfill.7f622118fedff873caa8.js.map
---
+++ 4.11小恐龙网页/static/setting.js
@@ -0,0 +1,15 @@
+const setting = {
+    "allPoint":100,
+    "percentage0":"Help Me~ ",
+    "percentage1":"已经过一小半了~ ",
+    "percentage2":"已经过一半了~ ",
+    "percentage3":"已经过一大半了~ ",
+    "percentage4":"拯救成功~ ",
+    "percentage0Num":0,
+    "percentage1Num":20,
+    "percentage2Num":50,
+    "percentage3Num":70,
+    "percentage4Num":100,
+    "countdown":2,
+    "countdownText":"Count Down"
+}
